ff3b0a822145f1f915c32182a1325bb5a064b027
Owo Sugiana <sugiana@gmail.com>
2011-04-05 01:00:03 +0700

    app.yml dikembalikan ke aslinya

--- urls.py
+++ urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls.defaults import *
+from django.contrib import admin
+admin.autodiscover()
 from msisdnarea import msisdn 
 
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
-    ('^_ah/warmup$', 'djangoappengine.views.warmup'),
+    #('^_ah/warmup$', 'djangoappengine.views.warmup'),
     #('^$', 'django.views.generic.simple.direct_to_template',
     # {'template': 'home.html'}),
     ('^$', msisdn),
+    ('^admin/', include(admin.site.urls)),
 )
