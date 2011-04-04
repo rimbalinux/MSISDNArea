b6cbe60474a40461c704772f9c04ece1741b033c
Owo Sugiana <sugiana@gmail.com>
2011-04-04 13:16:52 +0700

    django app engine

---
+++ urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls.defaults import *
+from msisdnarea import msisdn 
+
+handler500 = 'djangotoolbox.errorviews.server_error'
+
+urlpatterns = patterns('',
+    ('^_ah/warmup$', 'djangoappengine.views.warmup'),
+    #('^$', 'django.views.generic.simple.direct_to_template',
+    # {'template': 'home.html'}),
+    ('^$', msisdn),
+)
