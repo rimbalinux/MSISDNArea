6ea10c3c61336c0ec784eb85ec666f18af7b52a4
Owo Sugiana <sugiana@gmail.com>
2011-04-14 09:24:44 +0700

    login form menggunakan tag t

--- minicms/context_processors.py
+++ minicms/context_processors.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 def cms(request):
     return {
-        'site_name': settings.SITE_NAME + ' ' + request.session.get('lang','id'),
+        'site_name': settings.SITE_NAME,
         'site_copyright': settings.SITE_COPYRIGHT,
         'google_analytics_id': getattr(settings, 'GOOGLE_ANALYTICS_ID', None),
         'google_custom_search_id': getattr(settings, 'GOOGLE_CUSTOM_SEARCH_ID',
--- user/views.py
+++ user/views.py
@@ -3,16 +3,13 @@
 from django.contrib import auth
 from django.http import HttpResponseRedirect
 from django.views.generic.simple import direct_to_template
-from translate.lang import t
+from translate.lang import tr
 
 def login(request):
     if request.user.is_authenticated():
         return HttpResponseRedirect('/sudah-login-%s' % request.user.username)
     if not request.POST:
-        return direct_to_template(request, 'login.html', {
-            'username': t('Username', request),
-            'password': t('Password', request),
-            })
+        return direct_to_template(request, 'login.html')
     username = request.POST['username']
     password = request.POST['password']
     user = auth.authenticate(username=username, password=password) # just check
@@ -20,8 +17,7 @@ def login(request):
         auth.login(request, user) # session
         return HttpResponseRedirect('/')
     return direct_to_template(request, 'login.html', {
-        'error': t('Login gagal'),
-        't': t,
+        'error': tr('Login gagal', request),
         })
 
 def logout(request):
